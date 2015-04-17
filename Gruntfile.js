2846693406290a732644a35b234800ddaef39af3
sbrice <brices@gmail.com>
2015-04-16 21:48:47 -0700

    typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -300,7 +300,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: 'zestCleaningApp',
+        module: 'zestServicesApp',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
